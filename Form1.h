f85e2c89d81853a1ae65ad568a9a54fc60efe58c
Collin Ennis <collinennis@outlook.com>
2022-12-12 18:19:18 -0800

    Fixed button colors. Added clear output textbox button, added clear all fields button

--- Form1.h
+++ Form1.h
@@ -51,6 +51,8 @@ namespace CppCLRWinFormsProject {
 	private: System::Windows::Forms::Button^ button4;
 	private: System::Windows::Forms::Button^ button5;
 	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
 
 
 
@@ -84,21 +86,23 @@ namespace CppCLRWinFormsProject {
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(112, 86);
+			this->textBox1->Location = System::Drawing::Point(74, 58);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(319, 102);
+			this->textBox1->Size = System::Drawing::Size(314, 66);
 			this->textBox1->TabIndex = 0;
 			// 
 			// richTextBox1
 			// 
 			this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->richTextBox1->Location = System::Drawing::Point(74, 40);
+			this->richTextBox1->Location = System::Drawing::Point(47, 12);
 			this->richTextBox1->Name = L"richTextBox1";
 			this->richTextBox1->Size = System::Drawing::Size(397, 40);
 			this->richTextBox1->TabIndex = 1;
@@ -108,7 +112,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->richTextBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->richTextBox2->Location = System::Drawing::Point(74, 206);
+			this->richTextBox2->Location = System::Drawing::Point(47, 130);
 			this->richTextBox2->Name = L"richTextBox2";
 			this->richTextBox2->Size = System::Drawing::Size(397, 40);
 			this->richTextBox2->TabIndex = 2;
@@ -116,17 +120,17 @@ namespace CppCLRWinFormsProject {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(112, 252);
+			this->textBox2->Location = System::Drawing::Point(74, 176);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(319, 102);
+			this->textBox2->Size = System::Drawing::Size(314, 60);
 			this->textBox2->TabIndex = 3;
 			// 
 			// richTextBox3
 			// 
 			this->richTextBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->richTextBox3->Location = System::Drawing::Point(513, 40);
+			this->richTextBox3->Location = System::Drawing::Point(47, 242);
 			this->richTextBox3->Name = L"richTextBox3";
 			this->richTextBox3->Size = System::Drawing::Size(397, 40);
 			this->richTextBox3->TabIndex = 4;
@@ -134,40 +138,40 @@ namespace CppCLRWinFormsProject {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(556, 86);
+			this->textBox3->Location = System::Drawing::Point(74, 288);
 			this->textBox3->Multiline = true;
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(319, 102);
+			this->textBox3->Size = System::Drawing::Size(314, 57);
 			this->textBox3->TabIndex = 5;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(556, 252);
+			this->textBox4->Location = System::Drawing::Point(556, 88);
 			this->textBox4->Multiline = true;
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-			this->textBox4->Size = System::Drawing::Size(319, 102);
+			this->textBox4->Size = System::Drawing::Size(319, 502);
 			this->textBox4->TabIndex = 7;
 			// 
 			// richTextBox4
 			// 
 			this->richTextBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->richTextBox4->Location = System::Drawing::Point(658, 206);
+			this->richTextBox4->Location = System::Drawing::Point(556, 3);
 			this->richTextBox4->Name = L"richTextBox4";
-			this->richTextBox4->Size = System::Drawing::Size(217, 40);
+			this->richTextBox4->Size = System::Drawing::Size(319, 40);
 			this->richTextBox4->TabIndex = 6;
-			this->richTextBox4->Text = L"Money after gambling";
+			this->richTextBox4->Text = L"            Money after gambling";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(556, 213);
+			this->button1->Location = System::Drawing::Point(556, 49);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(96, 33);
 			this->button1->TabIndex = 8;
-			this->button1->Text = L"Run simulation";
+			this->button1->Text = L"Run";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
@@ -175,7 +179,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(74, 412);
+			this->button2->Location = System::Drawing::Point(74, 397);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(58, 47);
 			this->button2->TabIndex = 9;
@@ -187,9 +191,9 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->richTextBox5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->richTextBox5->Location = System::Drawing::Point(74, 360);
+			this->richTextBox5->Location = System::Drawing::Point(74, 351);
 			this->richTextBox5->Name = L"richTextBox5";
-			this->richTextBox5->Size = System::Drawing::Size(397, 40);
+			this->richTextBox5->Size = System::Drawing::Size(314, 40);
 			this->richTextBox5->TabIndex = 10;
 			this->richTextBox5->Text = L"Gamble amount multiplier on loss";
 			// 
@@ -197,7 +201,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(162, 412);
+			this->button3->Location = System::Drawing::Point(138, 397);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(58, 47);
 			this->button3->TabIndex = 11;
@@ -209,7 +213,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(331, 412);
+			this->button4->Location = System::Drawing::Point(266, 397);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(58, 47);
 			this->button4->TabIndex = 12;
@@ -221,7 +225,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(245, 412);
+			this->button5->Location = System::Drawing::Point(202, 397);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(58, 47);
 			this->button5->TabIndex = 13;
@@ -233,7 +237,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(413, 412);
+			this->button6->Location = System::Drawing::Point(330, 397);
 			this->button6->Name = L"button6";
 			this->button6->Size = System::Drawing::Size(58, 47);
 			this->button6->TabIndex = 14;
@@ -241,11 +245,37 @@ namespace CppCLRWinFormsProject {
 			this->button6->UseVisualStyleBackColor = true;
 			this->button6->Click += gcnew System::EventHandler(this, &Form1::button6_Click);
 			// 
+			// button7
+			// 
+			this->button7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button7->Location = System::Drawing::Point(74, 525);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(314, 57);
+			this->button7->TabIndex = 15;
+			this->button7->Text = L"Reset all fields";
+			this->button7->UseVisualStyleBackColor = true;
+			this->button7->Click += gcnew System::EventHandler(this, &Form1::button7_Click);
+			// 
+			// button8
+			// 
+			this->button8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button8->Location = System::Drawing::Point(779, 49);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(96, 33);
+			this->button8->TabIndex = 16;
+			this->button8->Text = L"Clear";
+			this->button8->UseVisualStyleBackColor = true;
+			this->button8->Click += gcnew System::EventHandler(this, &Form1::button8_Click);
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1019, 548);
+			this->ClientSize = System::Drawing::Size(942, 594);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
 			this->Controls->Add(this->button6);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
@@ -333,12 +363,34 @@ private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e
 }
 private: System::Void resetButtons() {
 	if (multiplierClicked) {
-		button2->BackColor = Color::Gray;
-		button3->BackColor = Color::Gray;
-		button4->BackColor = Color::Gray;
-		button5->BackColor = Color::Gray;
-		button6->BackColor = Color::Gray;
+		button2->UseVisualStyleBackColor = true;
+		button3->UseVisualStyleBackColor = true;
+		button4->UseVisualStyleBackColor = true;
+		button5->UseVisualStyleBackColor = true;
+		button6->UseVisualStyleBackColor = true;
 	}
+}
+private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+	button2->UseVisualStyleBackColor = true;
+	button3->UseVisualStyleBackColor = true;
+	button4->UseVisualStyleBackColor = true;
+	button5->UseVisualStyleBackColor = true;
+	button6->UseVisualStyleBackColor = true;
+	multiplierClicked = false;
+	counter = 0;
+	gambleMultiplier = 0;
+	initialGambleAmount = 0;
+	endingMoney = 0;
+	gambleAmount = 0;
+	startingMoney = 0;
+	runTimes = 0;
+	textBox1->Text = "";
+	textBox2->Text = "";
+	textBox3->Text = "";
+	textBox4->Text = "";
+}
+private: System::Void button8_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox4->Text = "";
 }
 };
 }
